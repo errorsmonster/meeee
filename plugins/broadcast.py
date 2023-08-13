2d61a04d3cde2b3e4186f5f0fcb4d60a5583a2df
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-08-13 17:15:58 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -1,4 +1,4 @@
-
+import logging
 from pyrogram import Client, filters
 import datetime
 import time
@@ -6,6 +6,48 @@ from database.users_chats_db import db
 from info import ADMINS
 from utils import broadcast_messages, broadcast_messages_group
 import asyncio
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import re
+import json
+import base64
+from database.ia_filterdb import get_skip_series, set_skip_series
+
+logger = logging.getLogger(__name__)
+
+
+
+@Client.on_message(filters.command('skipseries') & filters.user(ADMINS))
+async def skip_series_command(bot, message):
+    skip_series = get_skip_series()
+    toggle_text = "𝗗𝗜𝗦𝗔𝗕𝗟𝗘" if skip_series else "𝗘𝗡𝗔𝗕𝗟𝗘"
+    callback_data = "disable_series" if skip_series else "enable_series"
+    button = InlineKeyboardButton(toggle_text, callback_data=callback_data)
+    keyboard = InlineKeyboardMarkup([[button]])
+
+    await message.reply("☮️ ᴅɪsᴀʙʟᴇ sᴋɪᴘᴘɪɴɢ sᴇʀɪᴇs ☮️" if skip_series else "☯️ ᴇɴᴀʙʟᴇ sᴋɪᴘᴘɪɴɢ sᴇʀɪᴇs ☯️", reply_markup=keyboard)
+    #await message.reply(f"series skipping stats: ({skip_series})", reply_markup=keyboard)
+
+@Client.on_callback_query(filters.regex("^(disable_series|enable_series)$"))
+async def handle_callback(bot, callback_query):
+    skip_series = get_skip_series()
+
+    if callback_query.data == "enable_series":
+        set_skip_series(True)
+    elif callback_query.data == "disable_series":
+        set_skip_series(False)
+    
+ 
+
+    toggle_text = "𝗗𝗜𝗦𝗔𝗕𝗟𝗘" if skip_series else "𝗘𝗡𝗔𝗕𝗟𝗘"
+    callback_data = "disable_series" if skip_series else "enable_series"
+    button = InlineKeyboardButton(toggle_text, callback_data=callback_data)
+    keyboard = InlineKeyboardMarkup([[button]])
+
+    await callback_query.answer()
+    #await callback_query.message.edit_reply_markup(reply_markup=keyboard)
+    #Show the current value of skip_series in the message reply
+    await callback_query.message.edit_text("🎈 ᴅᴏɴᴇ!\n\n✖️sᴋɪᴘᴘɪɴɢ sᴇʀɪᴇs ᴅɪsᴀʙʟᴇᴅ\n\n🗂sᴇʀɪᴇs ᴡɪʟʟ ɴᴏᴛ ɢᴇᴛ sᴋɪᴘᴘᴇᴅ ᴡʜᴇɴ ɪɴᴅᴇxɪɴɢ" if skip_series else "🎈 ᴅᴏɴᴇ!\n\n✔️sᴋɪᴘᴘɴɢ sᴇʀɪᴇs ᴇɴᴀʙʟᴇᴅ \n\n🗂sᴇʀɪᴇs ᴡɪʟʟ ɢᴇᴛ sᴋɪᴘᴘᴇᴅ ᴡʜᴇɴ ɪɴᴅᴇxɪɴɢ")
+@Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
         
 @Client.on_message(filters.command("broadcast") & filters.user(ADMINS) & filters.reply)
 # https://t.me/GetTGLink/4178
@@ -42,27 +84,32 @@ async def verupikkals(bot, message):
     await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
-async def broadcast_group(bot, message):
-    groups = await db.get_all_chats()
+async def grp_brodcst(bot, message):
+    chats = await db.get_all_chats()
     b_msg = message.reply_to_message
     sts = await message.reply_text(
-        text='Broadcasting your messages To Groups...'
+        text='Broadcasting your messages...'
     )
     start_time = time.time()
-    total_groups = await db.total_chat_count()
+    total_chats = await db.total_chat_count()
     done = 0
     failed =0
 
     success = 0
-    async for group in groups:
-        pti, sh = await broadcast_messages_group(int(group['id']), b_msg)
+    async for chat in chats:
+        pti, sh = await broadcast_messages(int(chat['id']), b_msg)
         if pti:
             success += 1
-        elif sh == "Error":
+        elif pti == False:
+            if sh == "Blocked":
+                blocked+=1
+            elif sh == "Deleted":
+                deleted += 1
+            elif sh == "Error":
                 failed += 1
         done += 1
+        await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}")    
+            await sts.edit(f"Broadcast in progress:\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}")
-        
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Chats {total_chats}\nCompleted: {done} / {total_chats}\nSuccess: {success}\nFailed: {failed}")
