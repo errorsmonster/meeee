8e2d2b0ee2c3829c1a46e5de45e82d24f532b761
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-08-13 21:23:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -53,7 +53,7 @@ async def give_filter(client, message):
             settings = await get_settings(message.chat.id)
             try:
                 if settings['auto_ffilter']:
-                    await auto_filter(client, message)
+                    await auto_filter(client,query, message)
             except KeyError:
                 grpid = await active_connection(str(message.from_user.id))
                 await save_group_settings(grpid, 'auto_ffilter', True)
