37f61b9e5ae41eaf6175ede41bed6e692f3a959b
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2023-08-19 03:37:11 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1787,7 +1787,20 @@ async def auto_filter(client, msg, spoll=False):
             return
         if len(message.text) < 100:
             search = message.text
-            m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+           # m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+            m = await message.reply_text(f"<b><i> Searching for '{search}' 🔎</i></b>")
+            
+            async def update_searching_text():
+                while True:
+                    await asyncio.sleep(1)
+                    if m:
+                        dots = '.' * ((await asyncio.to_thread(time.time)) % 4 + 1)
+                        content = f"<b><i> Searching for '{search}'{dots}</i></b>"
+                        await m.edit_text(content)
+                    else:
+                        break
+            
+            asyncio.ensure_future(update_searching_text())
             search = search.lower()
             find = search.split(" ")
             search = ""
@@ -1818,7 +1831,20 @@ async def auto_filter(client, msg, spoll=False):
     else:
         message = msg.message.reply_to_message  # msg will be callback query
         search, files, offset, total_results = spoll
-        m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+       # m=await message.reply_text(f"<b><i> 𝖲𝖾𝖺𝗋𝖼𝗁𝗂𝗇𝗀 𝖿𝗈𝗋 '{search}' 🔎</i></b>")
+        m = await message.reply_text(f"<b><i> Searching for '{search}' 🔎</i></b>")
+        async def update_searching_text():
+            while True:
+                await asyncio.sleep(1)
+                if m:
+                    dots = '.' * ((await asyncio.to_thread(time.time)) % 4 + 1)
+                    content = f"<b><i> Searching for '{search}'{dots}</i></b>"
+                    await m.edit_text(content)
+                else:
+                    break
+            
+        asyncio.ensure_future(update_searching_text())
+        
         settings = await get_settings(message.chat.id)
         await msg.message.delete()
     # if 'is_shortlink' in settings.keys():
